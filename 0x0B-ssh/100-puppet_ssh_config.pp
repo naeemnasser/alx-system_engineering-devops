ae4f1979e048a54346143469f8a5cdf11cec3a9b
root <naeemnasser89@gmail.cm>
2024-07-03 17:53:11 -0700

    hard1

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-# make changes to our configuration file using file puppet
-# set up your client SSH configuration file so that you can connect to a server without typing a password.
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
-  }
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton',
-  }
+# Client configuration file (w/ Puppet)
+file {'/etc/ssh/shh_config':
+	ensure  => 'present',
+}
+file_line {'Turn off passwd auth':
+	path    => '/etc/ssh/ssh_config',
+	line    => 'PasswordAuthentication no',
+	match   => 'PasswordAuthentication yes',
+	replace => 'true',
+}
+file_line {'Declare identity file':
+	path    => '/etc/ssh/ssh_config',
+	line    => 'IdentityFile ~/.ssh/school',
+	match   => '^IdentityFile',
+	ensure  => 'present',
+}
