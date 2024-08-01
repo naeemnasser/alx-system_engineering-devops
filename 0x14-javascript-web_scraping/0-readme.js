9f83c644ebfc945b874dba2935a7604e6428bd35
root <naeemnasser89@gmail.cm>
2024-07-31 17:33:01 -0700

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
-#!/usr/bin/node
-
-const filePath = process.argv[2];
 const fs = require('fs');
-fs.readFile(filePath, 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
+const path = process.argv[2];
+
+fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
     console.log(data);
-  }
 });
+
